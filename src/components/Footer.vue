e176d71b397e8b8f5a16a5020950925b4149e08d
bekzodxudaybergenow <bekzodxudaybergenow@gmail.com>
2026-01-05 19:33:25 +0500

    Initial Commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<script setup>
+    import footerLogo from '@/assets/images/footer-logo.png'
+</script>
+
+<template>
+  <footer>
+    <img :src="footerLogo" alt="Footer logo">
+    Boxify &copy; 2025
+  </footer>
+</template>
+
+<style scoped>
+  footer {
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    gap:5px;
+  }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<script setup>
+    import logo from '@/assets/images/logo.png'
+</script>
+
+<template>
+  <header>
+    <img :src="logo" alt="Logo">
+  </header>
+</template>
+
+<style scoped>
+</style>
